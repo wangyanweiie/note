8bb09c2525e18361205e128a35507085b59f2bbf
wyw <740484522@qq.com>
2024-03-19 17:53:20 +0800

    添加mvc/mvp概念

--- 【06】Vue/source/1-VUE2/13-补充知识/全局事件总线/App.vue
+++ 【06】Vue/source/1-VUE2/13-补充知识/全局事件总线/App.vue
@@ -1,16 +1,16 @@
 <template>
-	<div class="app">
-		<h1>{{message}}</h1>
-		<School/>
-		<Student/>
-	</div>
+  <div class="app">
+    <h1>{{ message }}</h1>
+    <School />
+    <Student />
+  </div>
 </template>
 
 <script>
 // 全局事件总线$bus
 /******************************************
-1.作用: 组件间通信的方式,适用于任意组件间通信;
-2.main.js中安装全局事件总线 (this是vue实例vm)
+1.作用: 组件间通信的方式, 适用于任意组件间通信;
+2.main.js 中安装全局事件总线 (this 是 vue 实例 vm)
    new Vue({
    	......
    	beforeCreate() {
@@ -19,35 +19,35 @@
    }) 
 3.使用事件总线监听事件：
 （1）发送事件与数据：this.$bus.$emit('xxxx',data)
-（2）监听事件接收数据：A组件想接收数据,则在A组件中给$bus绑定自定义事件,事件的回调留在A组件自身;
-		如果不写事先在methods中定义好方法作为回调,回调函数需要写成箭头函数的形式;
+（2）监听事件接收数据：A 组件想接收数据, 则在 A 组件中给 $bus 绑定自定义事件, 事件的回调留在 A 组件自身;
+		如果不写事先在 methods 中定义好方法作为回调, 回调函数需要写成箭头函数的形式;
     mounted(){
       this.$bus.$on('xxxx', data=>{
 				......
 			})
     }
-（3）注意: 最好在beforeDestroy钩子中,用this.$bus.$off去解绑当前组件监听的事件;
+（3）注意: 最好在 beforeDestroy 钩子中, 用 this.$bus.$off 去解绑当前组件监听的事件;
 **************************************************************************/
 </script>
 <script>
-	//需要在Vue脚手架中运行
-	import Student from './components/Student'
-	import School from './components/School'
+//需要在Vue脚手架中运行
+import Student from "./components/Student";
+import School from "./components/School";
 
-	export default {
-		name:'App',
-		components:{School,Student},
-		data() {
-			return {
-				message:'你好阿！',
-			}
-		}
-	}
+export default {
+  name: "App",
+  components: { School, Student },
+  data() {
+    return {
+      message: "你好阿！",
+    };
+  },
+};
 </script>
 
 <style scoped>
-	.app{
-		background-color: gray;
-		padding: 5px;
-	}
+.app {
+  background-color: gray;
+  padding: 5px;
+}
 </style>
--- 【06】Vue/source/1-VUE2/13-补充知识/全局事件总线/main.js
+++ 【06】Vue/source/1-VUE2/13-补充知识/全局事件总线/main.js
@@ -1,16 +1,16 @@
-import Vue from 'vue'
-import App from './App.vue'
-Vue.config.productionTip = false	//关闭Vue的生产提示
+import Vue from "vue";
+import App from "./App.vue";
+Vue.config.productionTip = false; //关闭 Vue 的生产提示
 
-//方法一: 添加全局事件总线
-//Vue.prototype.$bus = new Vue()
+// 方法一: 添加全局事件总线
+// Vue.prototype.$bus = new Vue()
 
-//声明vue实例
+// 声明 vue 实例
 new Vue({
-	el:'#app',
-	render: h => h(App),
-	//方法二: 添加全局事件总线,this就是当前的vue实例;
-	beforeCreate() {
-		Vue.prototype.$bus = this
-	},
-})
+  el: "#app",
+  render: (h) => h(App),
+  // 方法二: 添加全局事件总线, this 就是当前的 vue 实例;
+  beforeCreate() {
+    Vue.prototype.$bus = this;
+  },
+});
